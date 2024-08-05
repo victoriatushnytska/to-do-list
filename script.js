20457947ced5ffeda3cad26c072b8ec48e788091
vic <116874558+victoriatushnytska@users.noreply.github.com>
2024-08-05 13:48:24 +0000

    final changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Retrieve todo from local storage or initialize an empty array
 let todo = JSON.parse(localStorage.getItem("todo")) || [];
 const todoInput = document.getElementById("todoInput");
 const todoList = document.getElementById("todoList");
@@ -6,7 +5,6 @@ const todoCount = document.getElementById("todoCount");
 const addButton = document.querySelector(".btn");
 const deleteButton = document.getElementById("deleteButton");
 
-// Initialize
 document.addEventListener("DOMContentLoaded", function () {
   addButton.addEventListener("click", addTask);
   todoInput.addEventListener("keydown", function (event) {
